dbcbd6dd4796789eca61590d40fd35bd123e7dae
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2023-09-12 10:55:54 +0200

    trzeci commit poprawki1111

--- src/ui/molecules/StarsItemList.tsx
+++ src/ui/molecules/StarsItemList.tsx
@@ -1,7 +1,8 @@
 import {StarsItem} from "@/ui/atoms/StarsItem";
 
 //lista gwiazdek
-export const StarsItemList = ({ numberOfStars }) => {
+
+export const StarsItemList = ({ numberOfStars }: {numberOfStars: number}) => {
     return (
         <div className="mb-3 mt-2 flex justify-center">
             {Array.from({ length: numberOfStars }).map((_, index) => (
