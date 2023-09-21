6cda1a2d00c37adc7e6ef6e4301530a6af26ba5e
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2023-09-21 13:18:22 +0200

    commit paginacja poprawki1111

--- src/app/products/[pageNumber]/page.tsx
+++ src/app/products/[pageNumber]/page.tsx
@@ -2,6 +2,17 @@ import {ProductList} from "@/ui/organisms/ProductList";
 import {getProductList, getProductsByPage} from "@/api/products";
 import {Pagination} from "@/ui/molecules/Pagination";
 
+export const generateStaticParams = async () => {
+    const products = await getProductList();
+    const pagesAmount = Math.ceil(products.length / 5);
+    const pages = []
+    for (let i = 1; i <= pagesAmount; i++) {
+        pages.push({ page: i.toString() });
+    }
+    return pages.slice(0, 5);
+
+}
+
 export default async function ProductsPaginationPage({params}:{params:{pageNumber:number}}) {
 
     const productInPage = 5;
