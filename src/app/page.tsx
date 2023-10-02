76c9b18a2ccbdc394d7c34ef6d46e3ded674c680
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2023-10-02 13:13:19 +0200

    poprawki do testów home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
-export default function HomePage () {
+import {SuggestedProductsList} from "@/ui/organisms/SuggestedProducts";
+import { getProductsSuggestedList} from "@/api/products";
+import React, {Suspense} from "react";
+
+export default async function HomePage() {
+    const suggestedProducts =
+        await getProductsSuggestedList('T-Shirts');
+
+
     return (
-        <div> Home Page </div>
+        <>
+            <div> Home Page</div>
+            <aside>
+                <Suspense fallback={"Ładowanie....."}>
+                    {suggestedProducts ? (
+                        <SuggestedProductsList products={suggestedProducts}/>
+                    ) : (
+                        <div>Brak sugerowanych produktów.</div>
+                    )}                </Suspense>
+            </aside>
+        </>
     )
 }
 
