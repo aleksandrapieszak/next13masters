872f7a8a15e773f486eaa988a0acdf99008d974d
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2023-10-07 11:56:03 +0200

    modul 4q

--- src/ui/organisms/Nav.tsx
+++ src/ui/organisms/Nav.tsx
@@ -188,15 +188,15 @@ export const Nav=({categories, quantity
                                         <div className="flex items-center lg:ml-8">
                                             {/* Cart */}
                                             <div className="ml-4 flow-root lg:ml-8">
-                                                <Link href="/cart" className="group -m-2 flex items-center p-2">
-                                                    <ShoppingBagIcon
-                                                        className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                                                        aria-hidden="true"
-                                                    />
-                                                    <span
-                                                        className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{quantity}</span>
-                                                    <span className="sr-only">items in cart, view bag</span>
-                                                </Link>
+                                                {/*<Link href="/cart" className="group -m-2 flex items-center p-2">*/}
+                                                {/*    <ShoppingBagIcon*/}
+                                                {/*        className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"*/}
+                                                {/*        aria-hidden="true"*/}
+                                                {/*    />*/}
+                                                {/*    <span*/}
+                                                {/*        className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{quantity}</span>*/}
+                                                {/*    <span className="sr-only">items in cart, view bag</span>*/}
+                                                {/*</Link>*/}
                                             </div>
                                         </div>
                                     </div>
