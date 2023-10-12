be9340b9f15e54154df2b488fcef17e5b3eb47d8
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2023-10-12 19:34:21 +0200

    modul 4 end poprawki d testów

--- src/ui/atoms/RatingSelect.tsx
+++ src/ui/atoms/RatingSelect.tsx
@@ -3,8 +3,12 @@ export const RatingSelect = () => {
 
     return (
         <>
-            <label htmlFor="rating-id">Rating</label>
+            <label htmlFor="rating-id"></label>
+            <div className="lg:w-1/4 w-full"> {/* <-- Dodane klasy tutaj */}
+                <div className="block text-sm font-medium leading-6 text-gray-900">
+                    <p className="block text-sm font-medium leading-6 text-gray-900">Rating</p>
             <select
+                className="relative w-full cursor-default rounded-md bg-white py-1.5 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 name="rating"
                 id="rating-id"
                 required
@@ -16,6 +20,8 @@ export const RatingSelect = () => {
                     </option>
                 ))}
             </select>
+                </div>
+            </div>
         </>
     );
 };
