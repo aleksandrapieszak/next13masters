97ef01382dc763fd3d9211874851c91630a36960
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2023-10-13 10:33:06 +0200

    modul 4 end poprawki d testów

--- src/ui/atoms/ProductPrice.tsx
+++ src/ui/atoms/ProductPrice.tsx
@@ -1,7 +1,7 @@
 export const ProductPrice = ({price}: { price: number }) => {
     return (
         <div
-            className=" inset-x-0 flex h-12 items-end justify-center overflow-hidden rounded-lg p-4 ">
+            className=" inset-x-0 flex h-12 items-end justify-center overflow-hidden rounded-lg p-4 " data-testId="product-price">
             <div className="relative  text-2xl font-semibold text-black ">{price} zł</div>
         </div>
     )
