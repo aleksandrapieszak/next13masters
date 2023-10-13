eee2cf8db189ff1fb9d94872ebff9e782026e197
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2023-10-13 10:23:05 +0200

    modul 4 end poprawki d testów

--- src/ui/atoms/SortSelect.tsx
+++ src/ui/atoms/SortSelect.tsx
@@ -19,14 +19,6 @@ export const SortSelect = () => {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
-    const currentSort = searchParams.get("sort");
-    let currentTestId = "";
-    if (currentSort && currentSort.includes("price")) {
-        currentTestId = "sort-by-price";
-    } else if (currentSort && currentSort.includes("rating")) {
-        currentTestId = "sort-by-rating";
-    }
-
     return (
         <div className="lg:w-1/4 w-full">
             <div className="block text-sm font-medium leading-6 text-gray-900">
@@ -37,7 +29,7 @@ export const SortSelect = () => {
                     className="relative w-full cursor-default rounded-md bg-white py-1.5 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     name="sort_by"
                     id="sort_by_id"
-                    value={currentSort || "Sort by"}
+                    value={"Sort by"}
                     onChange={(event) => {
                         const currentSortValue = event.target.value;
                         const currentSearchParams = new URLSearchParams(searchParams.toString());
